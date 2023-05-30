1d2c4566b907f889c9a9add75867c01fc0dceb82
Preedroos <preedroos@gmail.com>
2023-05-30 12:36:19 -0400

    Add basic Calculator

---
+++ script.js
@@ -0,0 +1,97 @@
+let displayValue = document.querySelector('.display').textContent;
+let firstNumber = '';
+let secondNumber = '';
+let operator = '';
+
+const buttons = document.querySelectorAll('.buttons__number');
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    displayValue == 0
+      ? updateDisplay(button.textContent)
+      : updateDisplay(displayValue + button.textContent);
+  });
+});
+
+const equalsButton = document.querySelector('.buttons__equal');
+equalsButton.addEventListener('click', () => {
+  result = operate(operator, firstNumber, displayValue);
+  updateDisplay(result);
+});
+
+const clearButton = document.querySelector('.buttons__clear');
+clearButton.addEventListener('click', clear);
+
+const addButton = document.querySelector('.buttons__add');
+addButton.addEventListener('click', () => {
+  processOperation('+');
+});
+
+const subtractButton = document.querySelector('.buttons__subtract');
+subtractButton.addEventListener('click', () => {
+  processOperation('-');
+});
+
+const multiplyButton = document.querySelector('.buttons__multiply');
+multiplyButton.addEventListener('click', () => {
+  processOperation('*');
+});
+
+const divideButton = document.querySelector('.buttons__divide');
+divideButton.addEventListener('click', () => {
+  processOperation('/');
+});
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return b != 0 ? a / b : 'Error';
+}
+
+function operate(operator, a, b) {
+  a = Number(a);
+  b = Number(b);
+  switch (operator) {
+    case '+':
+      return add(a, b);
+    case '-':
+      return subtract(a, b);
+    case '*':
+      return multiply(a, b);
+    case '/':
+      return divide(a, b);
+  }
+}
+
+function clear() {
+  displayValue = 0;
+  firstNumber = '';
+  secondNumber = '';
+  document.querySelector('.display').textContent = displayValue;
+}
+
+function updateDisplay(value) {
+  displayValue = value;
+  document.querySelector('.display').textContent = displayValue;
+}
+
+function processOperation(sign) {
+  if (firstNumber == '') {
+    firstNumber = displayValue;
+  } else {
+    secondNumber = displayValue;
+    firstNumber = operate(operator, firstNumber, secondNumber);
+    secondNumber = '';
+  }
+  operator = sign;
+  updateDisplay(0);
+}
